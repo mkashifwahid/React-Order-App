344417162bde1c341e86d6ea4ee40064fb4bd8b0
Muhammad Kashif <mkashif.aw@gmail.com>
2022-12-01 19:02:57 +0500

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Madni Booker App</a>
+      </header>
+      <main>List of Products</main>
+    </div>
+  );
+}
+
+export default App;
