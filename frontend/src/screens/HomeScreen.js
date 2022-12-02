f6c52c103bf2f52421021bb169e4f55de8091481
Muhammad Kashif <mkashif.aw@gmail.com>
2022-12-02 08:16:27 +0500

    add UseState & axiom

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+//import data from '../data';
+import axios from 'axios'
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(()=>{
+    const fetchData = async() => {
+      const result = await axios.get('/api/products')
+      setProducts(result.data)
+    };
+    fetchData();
+  }, [])
   return (
     <div>
       <h1> List of Products </h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.products.map((product) => (
           <div className="product" key={product.itemId}>
             <div className="product-info">
               <Link to={`/product/${product.itemId}`}>
