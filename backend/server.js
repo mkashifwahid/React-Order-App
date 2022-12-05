8cca7f000033f31f639ca43da744fa4061524180
Muhammad Kashif <mkashif.aw@gmail.com>
2022-12-05 20:26:00 +0500

    Finalize cart working and redirect to signin screen

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,30 @@ import data from './data.js';
 
 const app = express();
 
-app.get("/api/products", (req, res)=>{
-    res.send(data.products);
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
 });
 
-app.get("/api/products/itemId/:itemId", (req, res)=>{
-    const product = data.products.find((x) => x.itemId === req.params.itemId);
-    if(product){
-        res.send(product);
-    }
-    else{
-        res.status(404).send({message: 'Product not found'});
-    }
+app.get(`/api/products/itemId/:itemId`, (req, res) => {
+  const product = data.products.find((x) => x.itemId === req.params.itemId);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
 });
 
+app.get('/api/products/:itemId', (req, res) => {
+  const product = data.products.find((x) => x.itemId === req.params.itemId);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-    console.log(`serve at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
