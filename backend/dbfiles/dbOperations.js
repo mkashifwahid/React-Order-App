1d234629680fdd35f5045ad2363366ea92405ab5
Muhammad Kashif <mkashif.aw@gmail.com>
2022-12-09 20:44:09 +0500

    get data finally from server

--- backend/dbfiles/dbOperations.js
+++ backend/dbfiles/dbOperations.js
@@ -1,15 +1,15 @@
 import config from './dbConfig.js';
 import sql from 'mssql';
 
-const getProducts = async () => {
+async function getProducts() {
   try {
     let pool = await sql.connect(config);
-    let products = pool.request.require('exec sp_GetItem');
+    let products = pool.request().query('exec sp_GetItem');
     console.log(products);
     return products;
   } catch (error) {
     console.log(error);
   }
-};
+}
 
-export default { getProducts };
+export default getProducts;
