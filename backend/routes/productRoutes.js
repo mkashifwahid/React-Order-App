683b73954ef0ea7a5aea8067b155b734867eadb2
Muhammad Kashif <mkashif.aw@gmail.com>
2022-12-16 20:40:54 +0500

    try add collection

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import Product from '../models/productModel.js';
+
+const productRouter = express.Router();
+
+productRouter.get('/', async (req, res) => {
+  //await Product.remove({});
+  const result = await getProducts();
+
+  const products = await Product.findAll();
+  res.send({ products });
+});
+
+productRouter.get(`/Id/:Id`, async (req, res) => {
+  //const product = Product.findOne(x) => x.Id === req.params.Id);
+  const product = await Product.findOne({ where: { Id: req.params.Id } });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+});
+
+productRouter.get('/api/products/:Id', async (req, res) => {
+  const product = await Product.findOne({ where: { Id: req.params.Id } });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+});
+
+export default productRouter;
+
+// app.get('/api/products', async (req, res) => {
+//     console.log('connect to server');
+//     const result = await getProducts();
+//     res.send(result.recordset);
+//   });
